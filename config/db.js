7aa9fcdcb0a4eaf41ebceb46c9a4e077b0d9f624
imeer.ai <abbaszameer234@gmail.com>
2026-01-09 12:03:13 +0500

    Improve database connection handling

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
+// Async function to establish MongoDB connection
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
