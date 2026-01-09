9f07acfccbb61acf47a2275e835b2213be7a8576
imeer.ai <abbaszameer234@gmail.com>
2026-01-09 11:42:48 +0500

    Refactor server initialization

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 
+// Import configured Express application
 const app = require('./app');
 const PORT = process.env.PORT || 3000;
+// Start server and listen for incoming connections
 app.listen(PORT, () =>
   console.log(`🚀 IMEER.ai Server running on http://localhost:${PORT}`)
 );
