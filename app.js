921cfd2b52aae12e0ead63e8025fc4022f4b8b9a
imeer.ai <abbaszameer234@gmail.com>
2026-01-11 19:52:17 +0500

    cookie-paser set

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 const regRouter = require('./routes/index.routes');
 const engine = require('ejs-mate');
+const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -16,6 +17,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
 
 // ========== ROUTES ==========
 app.get('/', (req, res) => {
