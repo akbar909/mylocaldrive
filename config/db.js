8181f6b1a520dc6da7cfd717d6c6e2ee6464474b
imeer.ai <abbaszameer234@gmail.com>
2026-01-19 19:55:25 +0500

    config/db.js: Fix MongoDB URI environment variable name

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 // Async function to establish MongoDB connection
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
